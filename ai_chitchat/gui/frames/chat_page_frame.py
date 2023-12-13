754865172e1cfb9c4e3bd0452ddc5afac1d6fab0
runam00 <121118679+runam00@users.noreply.github.com>
2023-12-13 13:51:49 +0900

    チャットページに配置するフレームクラスを定義。中身は未実装

--- ai_chitchat/gui/frames/chat_page_frame.py
+++ ai_chitchat/gui/frames/chat_page_frame.py
@@ -4,6 +4,14 @@ class ChatPageFrame(ct.CTkFrame):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
 
-        # アプリタイトル
-        self.label = ct.CTkLabel(self, text='chat frame')
-        self.label.pack()
+
+class InterfaceFrame(ct.CTkFrame):
+    '''テキスト送信、音量調整、ダウンロードなどのユーザーインターフェースを配置するフレーム'''
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+
+
+class ChatHistoryFrame(ct.CTkFrame):
+    '''チャット履歴を表示するフレーム'''
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
